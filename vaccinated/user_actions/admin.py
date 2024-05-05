44b463e8a7446c921141bde8b65f37b407550068
mehmetsan <trabzonpower@gmail.com>
2024-05-05 19:16:06 +0200

    Initial models created

---
+++ vaccinated/user_actions/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.admin.exceptions import AlreadyRegistered
+from django.apps import apps
+
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
