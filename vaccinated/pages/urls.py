49164868147474d807e22048a3f5c0ad0f1843ac
mehmetsan <trabzonpower@gmail.com>
2024-06-11 02:17:44 +0200

    Last version

--- vaccinated/pages/urls.py
+++ vaccinated/pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from pages.views import home, sign_in_page, sign_out, sign_up_page
+from pages.views import home, sign_in_page, sign_out, sign_up_page, two_factor_page
 
 urlpatterns = [
     path("home", home, name="home"),
     path("sign_in", sign_in_page, name="sign_in"),
     path("sign_out", sign_out, name="sign_out"),
     path("sign_up", sign_up_page, name="sign_up"),
+    path("two_factor", two_factor_page, name="two_factor"),
 
 ]
